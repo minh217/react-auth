cba0df5abe1ae4a6a3e40ceee4b4ede2b1c03cfe
Hoàng Minh <minh.tang@mogi.vn>
2021-10-14 09:43:29 +0700

    Update source code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,10 @@
-import React, { useEffect, useState } from "react";
-const Home = () => {
+import React from "react";
+const Home = (props: {name:string}) => {
 
-    const [name, setName] = useState("");
-    useEffect(() => {
-        (
-            async () => {
-                const response = await fetch('http://localhost:8000/api/user', {
-                    method:"GET",
-                    headers:{'Content-Type':'application/json'},
-                    credentials: 'include' 
-                });
-                const content = await response.json();
 
-                setName(content.name);
-            }
-           
-
-        )();
-        
-    });
     return(
         <div>
-            Home {name}
+            {props.name ?  'Hi ' +  props.name : 'You are not logged in'}
         </div>
     );
 };
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,13 @@
 import React, { SyntheticEvent,useState } from "react";
+import { Redirect } from "react-router";
 
-const Login = () => {
+const Login = (props: { setName:(name:string) => void }) => {
     const [email, setEmail] = useState("");
     const [password,setPassword] = useState("");
+    const [redirect, setRedirect] = useState(false);
     const submit = async (e: SyntheticEvent) => {
         e.preventDefault();
-        await fetch('http://localhost:8000/api/login', {
+          const response = await fetch('http://localhost:8000/api/login', {
                 method: 'POST',
                 headers:{'Content-Type':'application/json'},
                 credentials: 'include',
@@ -14,7 +16,15 @@ const Login = () => {
                     password
                 })
             });    
+        const content = await response.json();
+        console.log(content);
+        props.setName(content.name);
+        
+        setRedirect(true);
     }
+    
+    if(redirect)
+        return <Redirect to="/"/>
     return(
         <form onSubmit={submit}>
             <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
